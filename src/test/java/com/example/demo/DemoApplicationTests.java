948dbfb4f3721fc78a431c160483d5cd8826a883
LucasM2010 <131216534+LucasM2010@users.noreply.github.com>
2024-10-07 15:26:04 -0300

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootTest
 class DemoApplicationTests {
